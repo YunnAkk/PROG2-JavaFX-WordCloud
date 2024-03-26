7c649d79aea0d9fb6c30d9a8ebf4fabd735e5ccc
Yun <161520228+YunnAkk@users.noreply.github.com>
2024-03-26 22:31:03 +0100

    Finished MainWindowController class

--- app/src/main/java/ch/zhaw/it/prog2/wordcloud/MainWindowController.java
+++ app/src/main/java/ch/zhaw/it/prog2/wordcloud/MainWindowController.java
@@ -1,8 +1,12 @@
 package ch.zhaw.it.prog2.wordcloud;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class MainWindowController {
 
@@ -10,13 +14,33 @@ public class MainWindowController {
     private Label labelTitle;
 
     @FXML
-    void addText(ActionEvent event) {
+    private TextField textEntry;
 
+    @FXML
+    private TextArea textHistory;
+
+    private StringProperty titleText;
+
+    public MainWindowController() {
+        titleText = new SimpleStringProperty();
+    }
+
+    @FXML
+    private void initialize() {
+        labelTitle.textProperty().bind(titleText);
+        titleText.bind(textEntry.textProperty());
+    }
+
+    @FXML
+    void addText(ActionEvent event) {
+        String text = textEntry.getText();
+        if (!text.isEmpty()) {
+            textHistory.appendText(text + "\n");
+        }
     }
 
     @FXML
     void clearTextEntry(ActionEvent event) {
-
+        textEntry.clear();
     }
-
 }
